36347ab5c9d2496330641aa986f6902f331b75aa
Your NameAnas-Nabih <anas7nabih@gmail.com>
2023-02-09 14:47:12 +0200

    add sizer package for responsive ui, change theme mode and add text in splash view.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,14 +1,25 @@
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/core/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:Utils.loadImage(img: AssetsData.logo),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Utils.loadImage(img: AssetsData.logo),
+        SizedBox(height: 3.h),
+        Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 14.sp),
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
- import 'package:bookly/core/constants.dart';
+import 'package:bookly/core/constants.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:bookly/res/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const BooklyApp());
 }
-
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: Const.appName,
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: MColors.kScaffoldBGColor
+    return Sizer(
+      builder: (context, orientation, deviceType) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: Const.appName,
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: MColors.kScaffoldBGColor),
+        home: const SplashView(),
       ),
-      home: const SplashView(),
     );
   }
 }
-
